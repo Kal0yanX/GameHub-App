32184901008e90177c3c95d6138fcf7a0e62e366
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-09-19 17:52:08 -0400

    removed console.logs from authentication

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,9 +4,8 @@ const user = require('../models/User')
 const jwt = require('json-web-token')
 
 router.post('/', async (req, res) => {
-    const User = await user.findOne({username: req.body.username})
-    console.log(req.body, User)
-    if (!User || !await bcrypt.compare(req.body.password, User.password)) {
+    const user = await User.findOne({ name: req.body.name })
+    if (!user || !await bcrypt.compare(req.body.password, user.password)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
         const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
