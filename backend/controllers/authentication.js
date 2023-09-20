d8bd3ce1a9a0910fbccb0497b077fa1540de0d03
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-09-19 20:40:45 -0400

    making sure login is working

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,8 +4,9 @@ const user = require('../models/User')
 const jwt = require('json-web-token')
 
 router.post('/', async (req, res) => {
-    const user = await User.findOne({ name: req.body.name })
-    if (!user || !await bcrypt.compare(req.body.password, user.password)) {
+    const User = await user.findOne({username: req.body.username})
+    console.log(req.body, User)
+    if (!user || !await bcrypt.compare(req.body.password, User.password)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
         const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
