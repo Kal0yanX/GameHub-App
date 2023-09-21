90da2dd82206cea6801664a9ffd69f9620b6cd1a
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-09-21 18:14:31 -0400

    rebase

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 export const CurrentUser = createContext()
--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import { useHistory, useParams } from "react-router"
 
 function SignUpForm() {
