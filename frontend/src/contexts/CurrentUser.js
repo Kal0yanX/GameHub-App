47ed7362e6650dedc4c2068b99e6752773c54729
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-09-21 13:39:35 -0400

    rebase

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createContext, useState, useEffect } from "react";
 
 
@@ -28,3 +29,22 @@ function CurrentUserProvider({ children }) {
 }
 
 export default CurrentUserProvider
+=======
+import { createContext, useState } from "react";
+
+
+export const CurrentUser = createContext()
+
+function CurrentUserProvider({ children }){
+
+    const [currentUser, setCurrentUser] = useState(null)
+
+    return (
+        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </CurrentUser.Provider>
+    )
+}
+
+export default CurrentUserProvider
+>>>>>>> dbd502c (rebase)
